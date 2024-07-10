2e566e586f8b8ad6f341154fbdf719708ef5fd51
Sherif-lotfy <s1h9e7r4if@gmail.com>
2024-07-10 09:25:44 +0300

    creating the db

---
+++ db.sql
@@ -0,0 +1,22 @@
+create database school;
+
+create table student (
+	studentId int primary key auto_increment,
+	fname varchar(20), 
+    lname varchar(20),
+    email varchar(20),
+    phone varchar(11)
+);
+
+create table courses (
+	courseCode varchar(10) primary key,
+    title varchar(20)
+);
+
+create table student_course (
+	grade varchar(5),
+    studentId int,
+    courseCode varchar(10),
+    foreign key (studentId) references student(studentId),
+    foreign key (courseCode) references courses(courseCode)
+);
